d324be54a79b573c3145d09af82c32cd4a1f3759
wedojava <wedojava@gmail.com>
2020-04-08 01:24:46 +0800

    fix build bug.

--- scripts/build.sh
+++ scripts/build.sh
@@ -20,21 +20,20 @@ echo "build start"
 for goos in "linux" "darwin" "freebsd" "windows"
     do
     # For AMD64
-    GOOS=$goos GOARCH=amd64
     out_f_name=$target_dir$target_file-$goos-amd64
     if [ "$goos" == "windows" ]; then
         out_f_name=$out_f_name.exe
     fi
-    go build -v -ldflags="-X main.VERSION=1.0.0 -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'" -o $out_f_name $main_file
+    GOOS=$goos GOARCH=amd64 go build -o $out_f_name $main_file
     tar JcvfP $target_dir/$target_file-$goos-amd64.tar.xz $out_f_name
     rm -f $out_f_name
     # For 386
-    GOOS=$goos GOARCH=386
+    
     out_f_name=$target_dir$target_file-$goos-386
     if [ "$goos" == "windows" ]; then
         out_f_name=$out_f_name.exe
     fi
-    go build -v -ldflags="-X main.VERSION=1.0.0 -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'" -o $out_f_name $main_file
+    GOOS=$goos GOARCH=386 go build -o $out_f_name $main_file
     tar JcvfP $target_dir/$target_file-$goos-386.tar.xz $out_f_name
     rm -f $out_f_name
 done
