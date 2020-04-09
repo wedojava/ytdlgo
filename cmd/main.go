cc1c4bd58b450bf824151616e040da02f7eb7349
wedojava <wedojava@gmail.com>
2020-04-10 01:53:46 +0800

    set Downloads as files save folder instead of date named folder

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"log"
 	"os"
 	"time"
 
-	"github.com/wedojava/ytdlgo/internal/commons"
 	"github.com/wedojava/ytdlgo/internal/ytdlgo"
 )
 
@@ -16,13 +14,13 @@ func main() {
 			server()
 		}
 		if os.Args[1] == "2" {
-			println("Download service start now!")
-			getNow()
+			println("Download once start right now!")
+			ytdlgo.DownloadConfOnce("", "", "Downloads")
 		}
 	}
 	if len(os.Args) == 1 {
 		println("Deal with the download list once right now...")
-		getNowDirectly()
+		ytdlgo.DownloadWatchLinks("list.txt", "", "Downloads")
 	}
 	os.Exit(3)
 }
@@ -32,36 +30,14 @@ func server() {
 		now := time.Now()
 		// every 15' start workflow
 		if now.Minute() == 15 {
-			root, err := commons.PathGenAsDate()
-			if err != nil {
-				log.Fatal(err)
-			}
-			ytdlgo.DownloadConfOnce("", "", root)
+			ytdlgo.DownloadConfOnce("", "", "Downloads")
 		} else {
 			time.Sleep(1 * time.Minute) // sleep 1 minute
 		}
-		err := commons.RemoveRoutine("")
-		if err != nil {
-			log.Fatal(err)
-		}
+		// TODO: Need to rewrite method below
+		// err := commons.RemoveRoutine("")
+		// if err != nil {
+		//         log.Fatal(err)
+		// }
 	}
-}
-
-func getNowDirectly() {
-	root, err := commons.PathGenAsDate()
-	if err != nil {
-		log.Fatal(err)
-	}
-	//ytdlgo.DownloadWatchLinks("", "", root)
-	//ytdlgo.DownloadWatchLinks("test/watchlist.txt", "", root)
-	ytdlgo.DownloadWatchLinks("list.txt", "", root)
-}
-
-func getNow() {
-	root, err := commons.PathGenAsDate()
-	if err != nil {
-		log.Fatal(err)
-	}
-	ytdlgo.DownloadConfOnce("", "", root)
-
 }
